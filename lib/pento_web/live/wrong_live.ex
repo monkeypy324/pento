93b02cfb8a52d3c9cf3d1e9364fe9ea8e72d1bde
monkeypy <monkeypy324@gmail.com>
2022-09-15 20:22:17 -0700

    Chapter 2

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,58 @@
+defmodule PentoWeb.WrongLive do
+  @moduledoc false
+
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+
+  alias Phoenix.LiveView.Socket
+
+  alias Pento.Accounts
+
+  @spec mount(any, any, Socket.t()) :: {:ok, Socket.t()}
+  def mount(_params, session, socket) do
+    user = Accounts.get_user_by_session_token(session["user_token"])
+
+    socket
+    |> assign(:score, 0)
+    |> assign(:message, "Make a guess: ")
+    |> assign(:actual, rand_num())
+    |> assign(:session_id, session["live_socket_id"])
+    |> assign(:current_user, user)
+    |> then(&{:ok, &1})
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number= {n} ><%= n %></a>
+      <% end %>
+      <pre>
+        <%= @current_user.email %>
+        <%= @session_id %>
+      </pre>
+    </h2>
+    """
+  end
+
+  @spec handle_event(binary(), map(), Socket.t()) :: {:noreply, Socket.t()}
+  def handle_event("guess", %{"number" => guess}, %Socket{assigns: %{actual: actual}} = socket)
+      when guess == actual do
+    message = "Correct. Try to guess the next number"
+    score = socket.assigns.score + 1
+    {:noreply, assign(socket, message: message, score: score, actual: rand_num())}
+  end
+
+  def handle_event("guess", %{"number" => guess}, %Socket{} = socket) do
+    message = "Your guess: #{guess}. Wrong. Guess Again."
+    score = socket.assigns.score - 1
+    {:noreply, assign(socket, message: message, score: score)}
+  end
+
+  defp rand_num do
+    1..10 |> Enum.random() |> to_string()
+  end
+end
